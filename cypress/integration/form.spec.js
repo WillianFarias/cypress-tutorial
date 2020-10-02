06daca48b5d6b7f46f7fa25f1b46eb197c16ce1d
WillianFarias <williansousafarias@gmail.com>
2020-10-02 05:16:17 -0300

    teste básico implementado

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -1,6 +1,12 @@
 describe("Form test", () => {
   it("Can fill the form", () =>{
+    //basta estar na tela para que o teste passe
     cy.visit("/");
     cy.get("form");
+
+
+    cy.get('input[name="name"]')
+      .type("Molly")
+      .should("have.value", "Molly");
   });
 });
